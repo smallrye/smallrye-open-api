3b9ab00ccca1cb9e13c466426f2043e36cd74476
Luca Molteni <volothamp@gmail.com>
2020-06-16 12:26:01 +0200

    Support java.time.Duration and java.time.Period with examples and tests

--- core/src/main/java/io/smallrye/openapi/runtime/util/TypeUtil.java
+++ core/src/main/java/io/smallrye/openapi/runtime/util/TypeUtil.java
@@ -70,6 +70,12 @@ public class TypeUtil {
     private static final TypeWithFormat DATE_FORMAT = TypeWithFormat.of(SchemaType.STRING).format(DataFormat.DATE).build();
     private static final TypeWithFormat DATE_TIME_FORMAT = TypeWithFormat.of(SchemaType.STRING).format(DataFormat.DATE_TIME)
             .build();
+    private static final TypeWithFormat DURATION_FORMAT = TypeWithFormat.of(SchemaType.STRING).format(DataFormat.DURATION)
+            .example("PT32S")
+            .build();
+    private static final TypeWithFormat PERIOD_FORMAT = TypeWithFormat.of(SchemaType.STRING).format(DataFormat.PERIOD)
+            .example("P1M")
+            .build();
     private static final TypeWithFormat TIME_FORMAT = TypeWithFormat.of(SchemaType.STRING).format(DataFormat.TIME)
             .externalDocumentation("As defined by 'full-time' in RFC3339",
                     "https://xml2rfc.ietf.org/public/rfc/html/rfc3339.html#anchor14")
@@ -130,6 +136,10 @@ public class TypeUtil {
         TYPE_MAP.put(DotName.createSimple(java.time.LocalDateTime.class.getName()), DATE_TIME_FORMAT);
         TYPE_MAP.put(DotName.createSimple(java.time.ZonedDateTime.class.getName()), DATE_TIME_FORMAT);
         TYPE_MAP.put(DotName.createSimple(java.time.OffsetDateTime.class.getName()), DATE_TIME_FORMAT);
+
+        // Duration
+        TYPE_MAP.put(DotName.createSimple(java.time.Duration.class.getName()), DURATION_FORMAT);
+        TYPE_MAP.put(DotName.createSimple(java.time.Period.class.getName()), PERIOD_FORMAT);
 
         // Time
         TYPE_MAP.put(DotName.createSimple(java.time.LocalTime.class.getName()), TIME_LOCAL_FORMAT);
@@ -776,6 +786,8 @@ public class TypeUtil {
         static final String BYTE = "byte";
         static final String DATE = "date";
         static final String DATE_TIME = "date-time";
+        static final String DURATION = "duration";
+        static final String PERIOD = "period";
         static final String URI = "uri";
         static final String UUID = "uuid";
         static final String TIME = "time";
--- extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/entities/GenericTypeTestContainer.java
+++ extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/entities/GenericTypeTestContainer.java
@@ -1,6 +1,8 @@
 package test.io.smallrye.openapi.runtime.scanner.entities;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.Date;
 
 /**
@@ -21,6 +23,12 @@ public class GenericTypeTestContainer {
 
     // Type containing a variety of collections and maps.
     GenericFieldTestContainer<String, LocalDateTime> genericContainer;
+
+    // Type containing a variety of collections against duration
+    GenericFieldTestContainer<String, Duration> durationContainer;
+
+    // Type containing a variety of collections against period
+    GenericFieldTestContainer<String, Period> periodContainer;
 
     // Type containing fields with overridden names.
     FieldNameOverride overriddenNames;
