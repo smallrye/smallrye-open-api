283fbae331943a9fe8b0329bb101d0e68b654d80
Phillip Kruger <phillip.kruger@gmail.com>
2020-07-15 15:55:48 +0200

    formatting

--- extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/resources/EmptySecurityRequirementsResource.java
+++ extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/resources/EmptySecurityRequirementsResource.java
@@ -2,6 +2,7 @@ package test.io.smallrye.openapi.runtime.scanner.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
 import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirements;
 
 @Path("/public")
