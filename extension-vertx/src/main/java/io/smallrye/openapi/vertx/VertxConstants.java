ac0e277a5ee583fc967702a329540acb0aea3eb8
Phillip Kruger <phillip.kruger@gmail.com>
2020-07-23 15:29:45 +0200

    Make sure Quarkus extension works Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- extension-vertx/src/main/java/io/smallrye/openapi/vertx/VertxConstants.java
+++ extension-vertx/src/main/java/io/smallrye/openapi/vertx/VertxConstants.java
@@ -9,12 +9,12 @@ import org.jboss.jandex.DotName;
  */
 public class VertxConstants {
 
-    static final DotName ROUTE = DotName.createSimple("io.quarkus.vertx.web.Route");
-    static final DotName ROUTE_BASE = DotName.createSimple("io.quarkus.vertx.web.RouteBase");
+    public static final DotName ROUTE = DotName.createSimple("io.quarkus.vertx.web.Route");
+    public static final DotName ROUTE_BASE = DotName.createSimple("io.quarkus.vertx.web.RouteBase");
 
-    static final DotName REQUEST_BODY = DotName.createSimple("io.quarkus.vertx.web.Body");
-    static final DotName PARAM = DotName.createSimple("io.quarkus.vertx.web.Param");
-    static final DotName HEADER_PARAM = DotName.createSimple("io.quarkus.vertx.web.Header");
+    public static final DotName REQUEST_BODY = DotName.createSimple("io.quarkus.vertx.web.Body");
+    public static final DotName PARAM = DotName.createSimple("io.quarkus.vertx.web.Param");
+    public static final DotName HEADER_PARAM = DotName.createSimple("io.quarkus.vertx.web.Header");
 
     private VertxConstants() {
     }
