52209b003a400e03fa4d0f79753f2fd5d1eeea56
Phillip Kruger <phillip.kruger@gmail.com>
2020-07-23 12:32:36 +0200

    initial support for Vertx (Quarkus) added. All basic PUT is working Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- extension-vertx/src/test/java/io/smallrye/openapi/runtime/scanner/VertxAnnotationScannerTest.java
+++ extension-vertx/src/test/java/io/smallrye/openapi/runtime/scanner/VertxAnnotationScannerTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import test.io.smallrye.openapi.runtime.scanner.entities.Greeting;
 import test.io.smallrye.openapi.runtime.scanner.resources.GreetingGetRoute;
 import test.io.smallrye.openapi.runtime.scanner.resources.GreetingPostRoute;
+import test.io.smallrye.openapi.runtime.scanner.resources.GreetingPutRoute;
 
 /**
  * Basic Vert.x annotation scanning
@@ -58,16 +59,16 @@ public class VertxAnnotationScannerTest extends VertxDataObjectScannerTestBase {
      * @throws IOException
      * @throws JSONException
      */
-    //    @Test
-    //    public void testBasicPutRouteDefinitionScanning() throws IOException, JSONException {
-    //        Index i = indexOf(GreetingPutController.class, Greeting.class);
-    //        OpenApiAnnotationScanner scanner = new OpenApiAnnotationScanner(emptyConfig(), i);
-    //
-    //        OpenAPI result = scanner.scan();
-    //
-    //        printToConsole(result);
-    //        assertJsonEquals("resource.testBasicRoutePutDefinitionScanning.json", result);
-    //    }
+    @Test
+    public void testBasicPutRouteDefinitionScanning() throws IOException, JSONException {
+        Index i = indexOf(GreetingPutRoute.class, Greeting.class);
+        OpenApiAnnotationScanner scanner = new OpenApiAnnotationScanner(emptyConfig(), i);
+
+        OpenAPI result = scanner.scan();
+
+        printToConsole(result);
+        assertJsonEquals("resource.testBasicRoutePutDefinitionScanning.json", result);
+    }
 
     /**
      * This test a basic, no OpenApi annotations, hello world service
