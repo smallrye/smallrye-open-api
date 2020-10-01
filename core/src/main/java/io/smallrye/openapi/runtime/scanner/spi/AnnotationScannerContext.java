f89d6a6bdcc0cd4bdaa4b2861780d7ec3bda2f1e
Michael Edgar <michael@xlate.io>
2020-10-01 07:58:17 -0400

    Allow reuse of a component response's status code on operations
    Conflicts:
    	core/src/main/java/io/smallrye/openapi/runtime/io/response/ResponseReader.java
    	core/src/main/java/io/smallrye/openapi/runtime/scanner/spi/AnnotationScannerContext.java

--- core/src/main/java/io/smallrye/openapi/runtime/scanner/spi/AnnotationScannerContext.java
+++ core/src/main/java/io/smallrye/openapi/runtime/scanner/spi/AnnotationScannerContext.java
@@ -3,9 +3,11 @@ package io.smallrye.openapi.runtime.scanner.spi;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.microprofile.openapi.models.OpenAPI;
 import org.jboss.jandex.IndexView;
 
 import io.smallrye.openapi.api.OpenApiConfig;
+import io.smallrye.openapi.api.models.OpenAPIImpl;
 import io.smallrye.openapi.runtime.scanner.AnnotationScannerExtension;
 import io.smallrye.openapi.runtime.scanner.FilteredIndexView;
 import io.smallrye.openapi.runtime.scanner.dataobject.AugmentedIndexView;
@@ -21,20 +23,23 @@ public class AnnotationScannerContext {
     private final List<AnnotationScannerExtension> extensions;
     private final OpenApiConfig config;
     private final ClassLoader classLoader;
+    private final OpenAPI openApi;
 
     public AnnotationScannerContext(FilteredIndexView index, ClassLoader classLoader,
             List<AnnotationScannerExtension> extensions,
-            OpenApiConfig config) {
+            OpenApiConfig config,
+            OpenAPI openApi) {
         this.index = index;
         this.augmentedIndex = AugmentedIndexView.augment(index);
         this.classLoader = classLoader;
         this.extensions = extensions;
         this.config = config;
+        this.openApi = openApi;
     }
 
     public AnnotationScannerContext(IndexView index, ClassLoader classLoader,
             OpenApiConfig config) {
-        this(new FilteredIndexView(index, config), classLoader, Collections.emptyList(), config);
+        this(new FilteredIndexView(index, config), classLoader, Collections.emptyList(), config, new OpenAPIImpl());
     }
 
     public FilteredIndexView getIndex() {
@@ -55,5 +60,9 @@ public class AnnotationScannerContext {
 
     public ClassLoader getClassLoader() {
         return classLoader;
+    }
+
+    public OpenAPI getOpenApi() {
+        return openApi;
     }
 }
