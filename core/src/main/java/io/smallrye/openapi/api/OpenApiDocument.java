d134c3e4c7e0e2d6ef8e34f63bfa80ecaec6af4a
Phillip Krüger <phillip.kruger@gmail.com>
2020-10-24 11:46:23 +0200

    Allow multiple filters.

--- core/src/main/java/io/smallrye/openapi/api/OpenApiDocument.java
+++ core/src/main/java/io/smallrye/openapi/api/OpenApiDocument.java
@@ -29,7 +29,7 @@ public class OpenApiDocument {
     private transient OpenAPI annotationsModel;
     private transient OpenAPI readerModel;
     private transient OpenAPI staticFileModel;
-    private transient OASFilter filter;
+    private transient List<OASFilter> filters = new ArrayList<>();
     private transient String archiveName;
 
     private transient OpenAPI model;
@@ -98,7 +98,9 @@ public class OpenApiDocument {
     }
 
     public void filter(OASFilter filter) {
-        set(() -> this.filter = filter);
+        if (filter != null) {
+            set(() -> this.filters.add(filter));
+        }
     }
 
     public void archiveName(String archiveName) {
@@ -162,10 +164,13 @@ public class OpenApiDocument {
      * @param model
      */
     private OpenAPI filterModel(OpenAPI model) {
-        if (model == null || filter == null) {
+        if (model == null || filters.isEmpty()) {
             return model;
         }
-        return FilterUtil.applyFilter(filter, model);
+        for (OASFilter filter : filters) {
+            model = FilterUtil.applyFilter(filter, model);
+        }
+        return model;
     }
 
     private void set(Runnable action) {
@@ -186,7 +191,7 @@ public class OpenApiDocument {
         annotationsModel = null;
         readerModel = null;
         staticFileModel = null;
-        filter = null;
+        filters.clear();
         archiveName = null;
     }
 
