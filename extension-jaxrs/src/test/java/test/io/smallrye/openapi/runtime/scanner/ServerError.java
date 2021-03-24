036f8c961f20cc8d0fa1b5e989994a0032f93e95
Phillip Kruger <phillip.kruger@gmail.com>
2021-03-24 23:45:30 +0200

    Added Jakarta tests to ApiResponseTests Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

---
+++ extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/ServerError.java
@@ -0,0 +1,7 @@
+package test.io.smallrye.openapi.runtime.scanner;
+
+public class ServerError {
+
+    String description;
+
+}
---
+++ extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/jakarta/Pet.java
@@ -0,0 +1,10 @@
+package test.io.smallrye.openapi.runtime.scanner.jakarta;
+
+import jakarta.json.JsonString;
+
+public class Pet {
+
+    String id;
+    JsonString name;
+
+}
