36de6b671a28ef7a393688065cbf90fd29652d7e
Phillip Kruger <phillip.kruger@gmail.com>
2021-03-24 16:19:36 +0200

    Added Jakarta tests to ResourceInheritanceTests Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

---
+++ extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/ExampleResource1.java
@@ -0,0 +1,29 @@
+package test.io.smallrye.openapi.runtime.scanner;
+
+import java.time.LocalDate;
+
+import javax.ws.rs.Path;
+
+@Path(value = "/hi")
+public class ExampleResource1 extends GenericResource implements Greetable {
+
+    String from;
+
+    @Override
+    public void setFromName(String from) {
+        this.from = from;
+    }
+
+    LocalDate date;
+
+    @Override
+    public void setGreetingDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String greet(GreetingBean bean) {
+        return "hi " + bean.name + ", from: " + from + "; on date: " + date;
+    }
+
+}
