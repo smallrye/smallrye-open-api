9742f9f04758f3ab5677f6353efba446bd3a4884
Phillip Kruger <phillip.kruger@gmail.com>
2021-03-24 22:29:52 +0200

    Added Jakarta tests to GenericModelTypesResourceTest Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

---
+++ extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/jakarta/BaseResource.java
@@ -0,0 +1,23 @@
+package test.io.smallrye.openapi.runtime.scanner.jakarta;
+
+import jakarta.ws.rs.core.Response;
+
+public abstract class BaseResource<T extends BaseModel> {
+
+    protected ResultList<T> getAll1() {
+        return new ResultList.ResultBuilder<T>().status(200).build();
+    }
+
+    protected Result<T> post1(T t) {
+        return new Result.ResultBuilder<T>().status(200).build();
+    }
+
+    protected Result<T> put1(T e) {
+        return new Result.ResultBuilder<T>().status(200).build();
+    }
+
+    protected Response delete1(T t) {
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+}
---
+++ extension-jaxrs/src/test/java/test/io/smallrye/openapi/runtime/scanner/jakarta/ResidentsResource.java
@@ -0,0 +1,52 @@
+package test.io.smallrye.openapi.runtime.scanner.jakarta;
+
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path(value = "/v1/residents")
+@Produces(value = MediaType.APPLICATION_JSON)
+@Consumes(value = MediaType.APPLICATION_JSON)
+public class ResidentsResource extends BaseResource<Residents> {
+
+    @GET
+    public ResultList<Residents> getAll(@QueryParam(value = "id") String id, @QueryParam(value = "limit") int limit,
+            @QueryParam(value = "offset") int offset, @QueryParam(value = "orderby") List<String> orderBy) {
+        return super.getAll1();
+    }
+
+    @POST
+    @RequestBody(content = @Content(schema = @Schema(implementation = Residents.class)))
+    @APIResponse(responseCode = "200", description = "Creates a new Chart")
+    public Result<Residents> post(Residents chart) {
+        return super.post1(chart);
+    }
+
+    @PUT
+    @RequestBody(content = @Content(schema = @Schema(implementation = Residents.class)))
+    @APIResponse(responseCode = "200", description = "Creates a new Chart")
+    public Result<Residents> put(Residents chart) {
+        return super.put1(chart);
+    }
+
+    @DELETE
+    @RequestBody(content = @Content(schema = @Schema(implementation = Residents.class)))
+    public Response delete(Residents chart) {
+        return super.delete1(chart);
+    }
+
+}
