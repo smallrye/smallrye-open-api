75d7b7cae4a0d826957cdcb48a50f36d85f4833f
Michael Edgar <michael@xlate.io>
2022-02-16 07:21:48 -0500

    Sort new import

--- core/src/main/java/io/smallrye/openapi/runtime/io/schema/SchemaReader.java
+++ core/src/main/java/io/smallrye/openapi/runtime/io/schema/SchemaReader.java
@@ -7,9 +7,9 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Locale;
 
 import org.eclipse.microprofile.openapi.models.media.Schema;
 import org.jboss.jandex.AnnotationInstance;
