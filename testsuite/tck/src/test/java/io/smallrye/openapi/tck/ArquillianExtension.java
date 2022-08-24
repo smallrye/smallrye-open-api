7f7256be8ada59047b2cf59886d51777a3e71f97
Andrew Rouse <anrouse@uk.ibm.com>
2022-08-24 12:04:12 +0100

    TCK runner: Update the archive using BeforeDeploy
    Previously an ApplicationArchiveProcessor was used to run the
    implementation and generate the OpenAPI document in the archive before
    deploying it. However, and ApplicationArchiveProcessor is only called
    for _testable_ archives (archives where tests run on the server),
    whereas to pass the TCK we need to process all the archives.
    
    Instead, use the BeforeDeploy event to update the archive which is
    called for every deployment.

--- testsuite/tck/src/test/java/io/smallrye/openapi/tck/ArquillianExtension.java
+++ testsuite/tck/src/test/java/io/smallrye/openapi/tck/ArquillianExtension.java
@@ -1,12 +1,11 @@
 package io.smallrye.openapi.tck;
 
-import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
 import org.jboss.arquillian.core.spi.LoadableExtension;
 
 public class ArquillianExtension implements LoadableExtension {
     @Override
     public void register(ExtensionBuilder extensionBuilder) {
-        extensionBuilder.service(ApplicationArchiveProcessor.class, DeploymentProcessor.class)
+        extensionBuilder.observer(DeploymentProcessor.class)
                 .observer(AfterDeployObserver.class);
     }
 }
