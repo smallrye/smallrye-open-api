f210c9b8fd31bb727f0b7d915c0a9189ddf26486
Tim Quinn <tim.quinn@oracle.com>
2022-11-11 12:01:08 -0600

    Removed straggler import

--- ui/open-api-ui/src/test/java/io/smallrye/openapi/ui/IndexCreatorTest.java
+++ ui/open-api-ui/src/test/java/io/smallrye/openapi/ui/IndexCreatorTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
