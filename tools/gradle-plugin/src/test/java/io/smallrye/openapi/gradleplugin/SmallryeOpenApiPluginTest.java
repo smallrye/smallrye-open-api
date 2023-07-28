4c75570b3075a9e0fc1ac4fb5137f4e2acf7ddbc
Fabio Burzigotti <fburzigo@redhat.com>
2023-07-28 19:42:13 +0200

    [issue 1511] - Maven/Gradle plugin, adding integration tests to verify the output file types filter configuration property

--- tools/gradle-plugin/src/test/java/io/smallrye/openapi/gradleplugin/SmallryeOpenApiPluginTest.java
+++ tools/gradle-plugin/src/test/java/io/smallrye/openapi/gradleplugin/SmallryeOpenApiPluginTest.java
@@ -142,6 +142,18 @@ class SmallryeOpenApiPluginTest {
     }
 
     @Test
+    void simpleProjectWithJustYamlOutput(@TempDir Path buildDir) throws Exception {
+        // "Simple" Gradle project
+        smokeProject(buildDir, false, SmallryeOpenApiPlugin.TASK_NAME, "YAML");
+    }
+
+    @Test
+    void simpleProjectWithJustJsonOutput(@TempDir Path buildDir) throws Exception {
+        // "Simple" Gradle project
+        smokeProject(buildDir, false, SmallryeOpenApiPlugin.TASK_NAME, "JSON");
+    }
+
+    @Test
     void quarkusProjectGenApiOnly(@TempDir Path buildDir) throws Exception {
         // Quarkus Gradle project, just call the generateOpenApiSpec task
         smokeProject(buildDir, true, SmallryeOpenApiPlugin.TASK_NAME);
@@ -154,6 +166,10 @@ class SmallryeOpenApiPluginTest {
     }
 
     void smokeProject(Path buildDir, boolean withQuarkus, String taskName) throws Exception {
+        smokeProject(buildDir, withQuarkus, taskName, "ALL");
+    }
+
+    void smokeProject(Path buildDir, boolean withQuarkus, String taskName, String outputFileTypeFilter) throws Exception {
         Files.write(buildDir.resolve("settings.gradle"),
                 singletonList("rootProject.name = 'smoke-test-project'"));
 
@@ -189,6 +205,7 @@ class SmallryeOpenApiPluginTest {
                         "  infoLicenseName.set(\"Apache 2.0\")",
                         "  infoLicenseUrl.set(\"http://www.apache.org/licenses/LICENSE-2.0.html\")",
                         "  operationIdStrategy.set(OperationIdStrategy.METHOD)",
+                        "  outputFileTypeFilter.set(\"" + outputFileTypeFilter + "\")",
                         "}"));
 
         Path javaDir = Paths.get("src/main/java/testcases");
@@ -222,51 +239,68 @@ class SmallryeOpenApiPluginTest {
 
         runGradleTask(buildDir, taskName, withQuarkus);
 
-        checkGeneratedFiles(buildDir);
+        checkGeneratedFiles(buildDir, outputFileTypeFilter);
 
-        checkJarContents(buildDir, withQuarkus);
+        checkJarContents(buildDir, withQuarkus, outputFileTypeFilter);
     }
 
-    private static void checkJarContents(Path buildDir, boolean withQuarkus) throws Exception {
+    private static void checkJarContents(Path buildDir, boolean withQuarkus, String expectedOutputFileType) throws Exception {
         runGradleTask(buildDir, "jar", withQuarkus);
 
         Path jarFile = buildDir.resolve("build/libs/smoke-test-project.jar");
         assertThat(jarFile).isRegularFile();
 
         try (ZipFile zipFile = new ZipFile(jarFile.toFile())) {
-            assertThat(zipFile.getEntry("my-openapi-schema-file.yaml")).isNotNull();
-            assertThat(zipFile.getEntry("my-openapi-schema-file.json")).isNotNull();
+            if ("ALL".equals(expectedOutputFileType) || "YAML".equals(expectedOutputFileType)) {
+                assertThat(zipFile.getEntry("my-openapi-schema-file.yaml")).isNotNull();
+            }
+            if ("ALL".equals(expectedOutputFileType) || "JSON".equals(expectedOutputFileType)) {
+                assertThat(zipFile.getEntry("my-openapi-schema-file.json")).isNotNull();
+            }
             assertThat(zipFile.getEntry("testcases/DummyJaxRs.class")).isNotNull();
             assertThat(zipFile.getEntry("META-INF/MANIFEST.MF")).isNotNull();
         }
     }
 
-    private static void checkGeneratedFiles(Path buildDir) throws IOException {
+    private static void checkGeneratedFiles(Path buildDir, String expectedOutputFileType) throws IOException {
         Path targetOpenapiDir = buildDir.resolve("build/generated/openapi");
         assertThat(targetOpenapiDir).isDirectory();
-        assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.yaml")).isRegularFile();
-        assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.json")).isRegularFile();
 
-        JsonNode root = new ObjectMapper().readValue(
-                targetOpenapiDir.resolve("my-openapi-schema-file.json").toUri().toURL(),
-                JsonNode.class);
-        assertThat(root.get("openapi").asText()).isEqualTo("3.0.2");
+        if ("ALL".equals(expectedOutputFileType)) {
+            assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.yaml")).isRegularFile();
+            assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.json")).isRegularFile();
 
-        JsonNode info = root.get("info");
-        assertThat(info).isNotNull();
-        assertThat(info.get("title").asText()).isEqualTo("Info Title");
-        assertThat(info.get("description").asText()).isEqualTo("Info Description");
-        assertThat(info.get("termsOfService").asText()).isEqualTo("Info TOS");
-        assertThat(info.get("version").asText()).isEqualTo("Info Version");
-        assertThat(info.get("contact").get("email").asText()).isEqualTo("Info Email");
-        assertThat(info.get("contact").get("name").asText()).isEqualTo("Info Contact");
-        assertThat(info.get("contact").get("url").asText())
-                .isEqualTo("https://github.com/smallrye/smallrye-open-api/issues/1231");
-        assertThat(info.get("license").get("name").asText()).isEqualTo("Apache 2.0");
-        assertThat(info.get("license").get("url").asText()).isEqualTo("http://www.apache.org/licenses/LICENSE-2.0.html");
+            JsonNode root = new ObjectMapper().readValue(
+                    targetOpenapiDir.resolve("my-openapi-schema-file.json").toUri().toURL(),
+                    JsonNode.class);
+            assertThat(root.get("openapi").asText()).isEqualTo("3.0.2");
 
-        JsonNode paths = root.get("paths");
-        assertThat(paths.get("/mypath").get("get").get("operationId").asText()).isEqualTo("dummyThing");
+            JsonNode info = root.get("info");
+            assertThat(info).isNotNull();
+            assertThat(info.get("title").asText()).isEqualTo("Info Title");
+            assertThat(info.get("description").asText()).isEqualTo("Info Description");
+            assertThat(info.get("termsOfService").asText()).isEqualTo("Info TOS");
+            assertThat(info.get("version").asText()).isEqualTo("Info Version");
+            assertThat(info.get("contact").get("email").asText()).isEqualTo("Info Email");
+            assertThat(info.get("contact").get("name").asText()).isEqualTo("Info Contact");
+            assertThat(info.get("contact").get("url").asText())
+                    .isEqualTo("https://github.com/smallrye/smallrye-open-api/issues/1231");
+            assertThat(info.get("license").get("name").asText()).isEqualTo("Apache 2.0");
+            assertThat(info.get("license").get("url").asText()).isEqualTo("http://www.apache.org/licenses/LICENSE-2.0.html");
+
+            JsonNode paths = root.get("paths");
+            assertThat(paths.get("/mypath").get("get").get("operationId").asText()).isEqualTo("dummyThing");
+        }
+
+        if ("YAML".equals(expectedOutputFileType)) {
+            assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.yaml")).isRegularFile();
+            assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.json")).doesNotExist();
+        }
+
+        if ("JSON".equals(expectedOutputFileType)) {
+            assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.yaml")).doesNotExist();
+            assertThat(targetOpenapiDir.resolve("my-openapi-schema-file.json")).isRegularFile();
+        }
     }
 
     private static void runGradleTask(Path buildDir, String taskName, boolean withQuarkus) {
