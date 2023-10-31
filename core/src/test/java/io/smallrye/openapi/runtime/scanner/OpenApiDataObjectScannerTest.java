303803232a3ea22f73466fdcf263ecb60ad36b8d
Peter Steiner <pe-st@users.noreply.github.com>
2023-10-31 15:43:56 +0100

    Reproduce issue #1565
    Branch off 3.5.2 as that's what included in Quarkus 3.4.x where I
    encountered the bug.
    
    Later I realised that it was already fixed in 3.6.x (Quarkus 3.5.0)

--- core/src/test/java/io/smallrye/openapi/runtime/scanner/OpenApiDataObjectScannerTest.java
+++ core/src/test/java/io/smallrye/openapi/runtime/scanner/OpenApiDataObjectScannerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.models.media.Schema;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.Index;
@@ -11,6 +12,7 @@ import org.jboss.jandex.IndexView;
 import org.jboss.jandex.Type;
 import org.jboss.jandex.Type.Kind;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -41,4 +43,40 @@ class OpenApiDataObjectScannerTest {
         assertEquals(itemFormat, out.getItems().getFormat());
     }
 
+    @Test
+    void testClassWithSchemaTypeString() throws IOException {
+        @org.eclipse.microprofile.openapi.annotations.media.Schema(
+                description = "Nested class",
+                type = SchemaType.STRING)
+        class N {
+        }
+
+        @org.eclipse.microprofile.openapi.annotations.media.Schema(
+                description = "Containing class")
+        class C {
+            N n;
+        }
+
+        IndexView index = Index.of(C.class, N.class);
+        AnnotationScannerContext contextCFirst = new AnnotationScannerContext(index, Thread.currentThread().getContextClassLoader(),
+                IndexScannerTestBase.emptyConfig());
+        AnnotationScannerContext contextNFirst = new AnnotationScannerContext(index, Thread.currentThread().getContextClassLoader(),
+                IndexScannerTestBase.emptyConfig());
+        Type cType = Type.create(DotName.createSimple(C.class), Kind.CLASS);
+        Type nType = Type.create(DotName.createSimple(N.class), Kind.CLASS);
+
+        // io.smallrye.openapi.runtime.scanner.OpenApiAnnotationScanner.processClassSchemas()
+        // calls OpenApiDataObjectScanner.process() for all annotated classes in 'index',
+        // and the order of the calls might not be deterministic.
+        // Try both orders for the two example classes and expect the same result
+        OpenApiDataObjectScanner.process(contextCFirst, cType);
+        OpenApiDataObjectScanner.process(contextCFirst, nType);
+
+        OpenApiDataObjectScanner.process(contextNFirst, nType);
+        OpenApiDataObjectScanner.process(contextNFirst, cType);
+
+        assertEquals(
+                contextCFirst.getOpenApi().getComponents().getSchemas().get("1N").getRef(),
+                contextNFirst.getOpenApi().getComponents().getSchemas().get("1N").getRef());
+    }
 }
