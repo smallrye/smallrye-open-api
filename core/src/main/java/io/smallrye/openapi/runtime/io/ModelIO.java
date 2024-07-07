9b8f41ae85f0b8bfd4bc70a88193c5a4c8829fb8
Michael Edgar <michael@xlate.io>
2024-07-07 15:31:04 -0400

    Allow null map values in ModelIO#toLinkedMap collector
    Signed-off-by: Michael Edgar <michael@xlate.io>
    Co-authored-by: Andrew Rouse <anrouse@uk.ibm.com>

--- core/src/main/java/io/smallrye/openapi/runtime/io/ModelIO.java
+++ core/src/main/java/io/smallrye/openapi/runtime/io/ModelIO.java
@@ -6,9 +6,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget;
@@ -139,7 +140,25 @@ public abstract class ModelIO<T, V, A extends V, O extends V, AB, OB> {
     }
 
     protected static <T> Collector<Map.Entry<String, T>, ?, Map<String, T>> toLinkedMap() {
-        return Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (v1, v2) -> v1, LinkedHashMap::new);
+        BiConsumer<Map<String, T>, Map.Entry<String, T>> accumulator = (map, entry) -> {
+            String k = entry.getKey();
+            T v = entry.getValue();
+
+            if (map.containsKey(k)) {
+                throw new IllegalStateException(String.format(
+                        "Duplicate key %s (attempted merging values %s and %s)",
+                        k, map.get(k), v));
+            }
+
+            map.put(k, v);
+        };
+
+        BinaryOperator<Map<String, T>> combiner = (m1, m2) -> {
+            m2.entrySet().forEach(entry -> accumulator.accept(m1, entry));
+            return m1;
+        };
+
+        return Collector.of(LinkedHashMap::new, accumulator, combiner);
     }
 
     public AnnotationInstance getAnnotation(AnnotationTarget target) {
