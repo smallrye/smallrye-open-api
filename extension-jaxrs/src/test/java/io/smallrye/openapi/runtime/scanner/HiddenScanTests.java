df442cce2c8b0dfaea6c0c72e05042eae9345113
Michael Edgar <michael@xlate.io>
2024-08-29 09:35:54 -0400

    fix: do not include abstract classes without an implementation
    Signed-off-by: Michael Edgar <michael@xlate.io>

--- extension-jaxrs/src/test/java/io/smallrye/openapi/runtime/scanner/HiddenScanTests.java
+++ extension-jaxrs/src/test/java/io/smallrye/openapi/runtime/scanner/HiddenScanTests.java
@@ -2,6 +2,8 @@ package io.smallrye.openapi.runtime.scanner;
 
 import java.io.IOException;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.models.OpenAPI;
 import org.jboss.jandex.Index;
 import org.json.JSONException;
@@ -32,5 +34,32 @@ class HiddenScanTests extends IndexScannerTestBase {
         test("hidden.components.json", test.io.smallrye.openapi.runtime.scanner.jakarta.FruitResource3.class,
                 test.io.smallrye.openapi.runtime.scanner.jakarta.HiddenFruit.class,
                 test.io.smallrye.openapi.runtime.scanner.jakarta.VisibleFruit.class);
+    }
+
+    @jakarta.ws.rs.Path("/")
+    interface FooResource {
+        @jakarta.ws.rs.GET
+        @jakarta.ws.rs.Path("/foo")
+        @jakarta.ws.rs.Produces(jakarta.ws.rs.core.MediaType.TEXT_PLAIN)
+        @Operation(summary = "Gets the foo")
+        @APIResponse(responseCode = "200", description = "The Foo")
+        jakarta.ws.rs.core.Response getFoo();
+    }
+
+    @Test
+    void testIgnoredAbstractResource() throws IOException, JSONException {
+        @jakarta.ws.rs.Path("/")
+        abstract class BaseBarResource {
+            @jakarta.ws.rs.GET
+            @jakarta.ws.rs.Path("/bar")
+            @jakarta.ws.rs.Produces(jakarta.ws.rs.core.MediaType.TEXT_PLAIN)
+            @Operation(summary = "Gets the bar")
+            @APIResponse(responseCode = "200", description = "The Bar")
+            public jakarta.ws.rs.core.Response getBar() {
+                return null;
+            }
+        }
+
+        test("default.json", FooResource.class, BaseBarResource.class);
     }
 }
