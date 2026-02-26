b1aa07c99b7eef1d83f750f0be076168c8a58cc5
Michael Edgar <michael@xlate.io>
2026-02-26 05:54:47 -0500

    Remove open-api-ui-forms module
    Signed-off-by: Michael Edgar <michael@xlate.io>

--- ui/open-api-ui-forms/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MyOpenAPIForm from "./App";
-
-
-ReactDOM.render(
-    <MyOpenAPIForm />,
-    document.getElementById('rrr')
-  );
