579efbd953fc7ff8535656c4abef7f2e8b0c2ad9
Michael Davis <michael.davis@insurity.com>
2026-06-11 18:34:15 -0400

    style: avoid wildcard in import and tweak new log
    PR feedback

--- extension-spring/src/main/java/io/smallrye/openapi/spring/SpringAnnotationScanner.java
+++ extension-spring/src/main/java/io/smallrye/openapi/spring/SpringAnnotationScanner.java
@@ -1,6 +1,13 @@
 package io.smallrye.openapi.spring;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -163,7 +170,7 @@ public class SpringAnnotationScanner extends AbstractAnnotationScanner {
                 ClassInfo classInfo = annotationInstance.target().asClass();
                 applications.add(classInfo);
             } else {
-                SpringLogging.log.ignoringAnnotation(SpringConstants.REQUEST_MAPPING.withoutPackagePrefix());
+                SpringLogging.log.ignoringMultiTargetAnnotation(SpringConstants.REQUEST_MAPPING.withoutPackagePrefix());
             }
         }
 
